162b6e11abd14800367a30fc415e62fbe7b42f77
terodox <terodox@gmail.com>
2019-10-04 19:41:01 -0400

    Add tests

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,4 +1,5 @@
-import coerce from './index';
+import * as CoerceModule from './index';
+import { coerce, coerceArray } from './index';
 
 const ERROR_MESSAGE = 'You failing the hardest';
 class FakeClassForTesting {}
@@ -36,5 +37,39 @@ describe('coerce', () => {
 
         const errorMessageRegex = new RegExp(`${errorMessage}.*${ERROR_MESSAGE}`);
         expect(() => coerce({}, FailingClassForTesting, errorMessage)).toThrowError(errorMessageRegex);
+    });
+});
+
+describe('coerceArray', () => {
+    it('should be a function', () => {
+        expect(coerceArray).toEqual(jasmine.any(Function));
+    });
+
+    it('should throw if values is not an array', () => {
+        expect(() => coerceArray({}, FakeClassForTesting, 'Yay!')).toThrowError(/Array/);
+    });
+
+    it('should coerce every value in the array', () => {
+        const values = [
+            { hooray: 'yes'},
+            { oh: 'no'},
+            { OHHH: 'YEAHHH!!'}
+        ];
+
+        const result = coerceArray(values, FakeClassForTesting, 'Yay!');
+
+        expect(result).toEqual(jasmine.arrayContaining([
+            jasmine.any(FakeClassForTesting),
+            jasmine.any(FakeClassForTesting),
+            jasmine.any(FakeClassForTesting)
+        ]));
+    });
+
+    it('should throw if coercion fails', () => {
+        const values = [
+            { hooray: 'yes'}
+        ];
+
+        expect(() => coerceArray(values, FailingClassForTesting, 'Yay!')).toThrow();
     });
 });
