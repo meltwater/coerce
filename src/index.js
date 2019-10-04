8304b88d9a1063e792cd5c422f2386aaa226e399
terodox <terodox@gmail.com>
2019-10-04 19:31:04 -0400

    Add coerceArray method

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@
  * @returns {*} - Instance of provided Type
  * @throws {TypeError} - If the value is not coercable.
  */
-export default function coerce (value, Type, message) {
+export function coerce (value, Type, message) {
     if (value instanceof Type) {
         return value;
     }
@@ -17,4 +17,22 @@ export default function coerce (value, Type, message) {
     catch (error) {
         throw new TypeError(`${message} Inner Error: ${error.message}`);
     }
+}
+export default coerce;
+
+/**
+ * Validate an array of objects are an instance of the expected type, or is are a valid constructor object for the requested type
+ * @param {Array<*>} values - The array of values for validation
+ * @param {*} Type - The type for instance of comparison
+ * @param {string} message - The message to be displayed if coercion fails
+ * @returns {Array<*>} - Array of instances of provided Type
+ * @throws {TypeError} - If values is not an array
+ * @throws {TypeError} - One of the array values is not coercable.
+ */
+export function coerceArray (values, Type, message) {
+    if(!Array.isArray(values)) {
+        throw new TypeError(`values must be an Array. Provided value: ${values}`);
+    }
+
+    return values.map(value => coerce(value, Type, message));
 }
