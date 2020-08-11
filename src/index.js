641626dc5a45997ffcd7de9b35a75723ba603d49
terodox <terodox@gmail.com>
2020-08-11 15:08:23 -0400

    Add two new methods to allow a logical default error message based on parameterName

--- src/index.js
+++ src/index.js
@@ -2,11 +2,36 @@
  * Validate an object is an instance of the expected type, or is a valid constructor object for the requested type
  * @param {*} value - The value for validation
  * @param {*} Type - The type for instance of comparison
+ * @param {string} parameterName - The name of the parameter being coerced
+ * @returns {*} - Instance of provided Type
+ * @throws {TypeError} - If the value is not coercable.
+ */
+export function coerceParameter(value, Type, parameterName) {
+    return coerce(value, Type, `${parameterName} should be coerceable to ${Type.name}. Provided value: ${JSON.stringify(value, null, 2)}`)
+}
+
+/**
+ * Validate an array of objects are an instance of the expected type, or are a valid constructor object for the requested type
+ * @param {Array<*>} values - The array of values for validation
+ * @param {*} Type - The type for instance of comparison
+ * @param {string} parameterName - The name of the array typed parameter being coerced
+ * @returns {Array<*>} - Array of instances of provided Type
+ * @throws {TypeError} - If values is not an array
+ * @throws {TypeError} - One of the array values is not coercable.
+ */
+export function coerceArrayParameter(values, Type, parameterName) {
+    return coerceArray(values, Type, `${parameterName} should be an Array of items coerceable to ${Type.name}. Provided value: ${JSON.stringify(values, null, 2)}`)
+}
+
+/**
+ * Validate an object is an instance of the expected type, or is a valid constructor object for the requested type
+ * @param {*} value - The value for validation
+ * @param {*} Type - The type for instance of comparison
  * @param {string} message - The message to be displayed if coercion fails
  * @returns {*} - Instance of provided Type
  * @throws {TypeError} - If the value is not coercable.
  */
-export function coerce (value, Type, message) {
+export function coerce(value, Type, message) {
     if (value instanceof Type) {
         return value;
     }
@@ -29,8 +54,8 @@ export default coerce;
  * @throws {TypeError} - If values is not an array
  * @throws {TypeError} - One of the array values is not coercable.
  */
-export function coerceArray (values, Type, message) {
-    if(!Array.isArray(values)) {
+export function coerceArray(values, Type, message) {
+    if (!Array.isArray(values)) {
         throw new TypeError(`values must be an Array. Provided value: ${values}`);
     }
 
--- src/index.spec.js
+++ src/index.spec.js
@@ -1,12 +1,13 @@
-import { coerce, coerceArray } from './index';
+import { coerce, coerceArray, coerceArrayParameter, coerceParameter } from './index';
 
 const ERROR_MESSAGE = 'You failing the hardest';
-class FakeClassForTesting {}
+class FakeClassForTesting { }
 class FailingClassForTesting {
     constructor() {
         throw new Error(ERROR_MESSAGE);
     }
 }
+
 describe('coerce', () => {
     it('should be a function', () => {
         expect(coerce).toEqual(jasmine.any(Function));
@@ -50,9 +51,9 @@ describe('coerceArray', () => {
 
     it('should coerce every value in the array', () => {
         const values = [
-            { hooray: 'yes'},
-            { oh: 'no'},
-            { OHHH: 'YEAHHH!!'}
+            { hooray: 'yes' },
+            { oh: 'no' },
+            { OHHH: 'YEAHHH!!' }
         ];
 
         const result = coerceArray(values, FakeClassForTesting, 'Yay!');
@@ -66,9 +67,74 @@ describe('coerceArray', () => {
 
     it('should throw if coercion fails', () => {
         const values = [
-            { hooray: 'yes'}
+            { hooray: 'yes' }
         ];
 
         expect(() => coerceArray(values, FailingClassForTesting, 'Yay!')).toThrow();
+    });
+});
+
+describe('coerceArrayParameter', () => {
+    it('should be a function', () => {
+        expect(coerceArrayParameter).toEqual(jasmine.any(Function));
+    });
+
+    it('should throw if values is not an array', () => {
+        expect(() => coerceArrayParameter({}, FakeClassForTesting, 'Yay!')).toThrowError(/Array/);
+    });
+
+    it('should coerce every value in the array', () => {
+        const values = [
+            { hooray: 'yes' },
+            { oh: 'no' },
+            { OHHH: 'YEAHHH!!' }
+        ];
+
+        const result = coerceArrayParameter(values, FakeClassForTesting, 'Yay!');
+
+        expect(result).toEqual(jasmine.arrayContaining([
+            jasmine.any(FakeClassForTesting),
+            jasmine.any(FakeClassForTesting),
+            jasmine.any(FakeClassForTesting)
+        ]));
+    });
+
+    it('should throw if coercion fails', () => {
+        const parameterName = 'valuesOfGoodness';
+        const values = [
+            { hooray: 'yes' }
+        ];
+
+        expect(() => coerceArrayParameter(values, FailingClassForTesting, parameterName)).toThrowError(new RegExp(parameterName));
+    });
+});
+
+describe('coerceParameter', () => {
+    it('should be a function', () => {
+        expect(coerceParameter).toEqual(jasmine.any(Function));
+    });
+
+    it('should return value if it is an instanceof Type', () => {
+        const value = 'strings are fun';
+
+        expect(coerceParameter(value, String, '')).toEqual(value);
+    });
+
+    it('should cast into the type and return Typed version', () => {
+        const value = {};
+
+        expect(coerceParameter(value, FakeClassForTesting, '')).toEqual(jasmine.any(FakeClassForTesting));
+    });
+
+    it('should cast into the type and return Typed version', () => {
+        const parameterName = 'theBestParameter';
+        expect(() => coerceParameter({}, FailingClassForTesting, parameterName)).toThrowError(parameterName);
+    });
+
+    it('should add inner message to error message', () => {
+        const parameterName = 'theBestParameter';
+
+        const errorMessageRegex = new RegExp(`${parameterName}.*${ERROR_MESSAGE}`);
+        expect(() => coerceParameter({}, FailingClassForTesting, parameterName)).toThrowError(errorMessageRegex);
     });
 });
